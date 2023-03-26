30cab96548bfcf1f80bfb3233a492a7f6919a190
Paul Breen <pbree@bas.ac.uk>
2023-03-26 20:04:38 +0100

    Bump version

--- tests/test_xcsv_plot_map.py
+++ tests/test_xcsv_plot_map.py
@@ -9,7 +9,7 @@ import xcsv.plot_map as xpm
 base = os.path.dirname(__file__)
 
 def test_version():
-    assert xpm.__version__ == '0.1.0'
+    assert xpm.__version__ == '0.2.0'
 
 @pytest.fixture
 def short_test_datasets():
